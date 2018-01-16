2cd26e2369a5071e718904cafa309a972e2d0d44
Åke Johansson <akejo@telia.com>
2018-01-16 19:19:21 +0100

    Some cleaning up

--- ris.py
+++ ris.py
@@ -5,33 +5,34 @@ https://github.com/ml4a/ml4a-guides/blob/f31929024c51bdc409b52e91a77725291fa1656
 """
 import argparse
 import logging
+import fnmatch
 import os
 import random
 import cPickle as pickle
 import numpy as np
 import matplotlib.pyplot
-from matplotlib.pyplot import imshow
-import keras
-from keras.preprocessing import image
-from keras.applications.imagenet_utils import decode_predictions, preprocess_input
-from keras.models import Model
-from sklearn.decomposition import PCA
-from scipy.spatial import distance
-from tqdm import tqdm
+import keras.preprocessing.image
+import keras.applications.imagenet_utils
+import keras.models
+import sklearn.decomposition
+import scipy.spatial
+import tqdm
+import PIL
+
 
 logger = logging.getLogger(__name__)
 
 
 def get_image(path, target_size):
-    img = image.load_img(path, target_size=target_size)
-    x = image.img_to_array(img)
+    img = keras.preprocessing.image.load_img(path, target_size=target_size)
+    x = keras.preprocessing.image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
+    x = keras.applications.imagenet_utils.preprocess_input(x)
     return img, x
 
 def get_feature_extractor():
     model = keras.applications.VGG16(weights='imagenet', include_top=True)   
-    feat_extractor = Model(inputs=model.input, outputs=model.get_layer("fc2").output)
+    feat_extractor = keras.models.Model(inputs=model.input, outputs=model.get_layer("fc2").output)
     return feat_extractor
 
 def find_images(images_path, max_num_images=0):
@@ -44,13 +45,13 @@ def find_images(images_path, max_num_images=0):
 
 def _extract_pca_features(feat_extractor, images, n_components=300):
     features = []
-    for image_path in tqdm(images):
+    for image_path in tqdm.tqdm(images):
         img, x = get_image(image_path, target_size=feat_extractor.input_shape[1:3])
         feat = feat_extractor.predict(x)[0]
         features.append(feat)
 
     features = np.array(features)
-    pca = PCA(n_components=n_components)
+    pca = sklearn.decomposition.PCA(n_components=n_components)
     pca.fit(features)
     pca_features = pca.transform(features)
     return pca_features
@@ -58,7 +59,7 @@ def _extract_pca_features(feat_extractor, images, n_components=300):
 def get_pca_features(images_path, pca_features_file):
     if os.path.exists(pca_features_file):
         logger.debug('Features file already exists. Loading {} and ignoring images path {}'.
-                     format(pca_features_file, pca_features_file, images_path))
+                     format(pca_features_file, images_path))
         images, pca_features = pickle.load(open(pca_features_file, 'r'))
     else:
         images = find_images(images_path)
@@ -71,40 +72,51 @@ def get_pca_features(images_path, pca_features_file):
     return images, pca_features    
 
 def get_closest_images(pca_features, query_image_idx, num_results=5):
-    distances = [distance.euclidean(pca_features[query_image_idx], feat) for feat in pca_features ]
+    distances = [scipy.spatial.distance.euclidean(pca_features[query_image_idx], feat) for feat in pca_features ]
     idx_closest = sorted(range(len(distances)), key=lambda k: distances[k])[1:num_results+1]
     return idx_closest
 
 def get_concatenated_images(images, indexes, thumb_height):
+    border = PIL.Image.fromarray(np.full([thumb_height, thumb_height/10, 3], fill_value=255, dtype=np.int8), 'RGB')
     thumbs = []
     for idx in indexes:
-        img = image.load_img(images[idx])
+        img = keras.preprocessing.image.load_img(images[idx])
         img = img.resize((int(img.width * thumb_height / img.height), thumb_height))
         thumbs.append(img)
+        if len(thumbs) == 1:
+            thumbs.append(border)
     concat_image = np.concatenate([np.asarray(t) for t in thumbs], axis=1)
     return concat_image
 
 def show_closest_images(pca_features, images, query_image_idx):
     idx_closest = get_closest_images(pca_features, query_image_idx)
-    query_image = get_concatenated_images(images, [query_image_idx], 300)
-    results_image = get_concatenated_images(images, idx_closest, 200)
+    results_image = get_concatenated_images(images, [query_image_idx] + idx_closest, 300)
     
-    logger.debug('Closest to {}: {}'.format(images[query_image_idx], [images[x] + '\n' for x in idx_closest]))
+    logger.debug('Closest to {}: {}'.format(images[query_image_idx], [images[x] for x in idx_closest]))
 
-    # display the query image
-    matplotlib.pyplot.figure(figsize = (5,5))
-    imshow(query_image)
-    matplotlib.pyplot.title("query image (%d)" % query_image_idx)
+    print('Closest to {}'.format(images[query_image_idx]))
+    for x in idx_closest:
+        print('  {}'.format(images[x]))
 
-    # display the resulting images
+    # display the query and resulting images
     matplotlib.pyplot.figure(figsize = (16,12))
-    imshow(results_image)
-    matplotlib.pyplot.title("result images")
+    matplotlib.pyplot.imshow(results_image)
+    matplotlib.pyplot.title("Query image and result images")
 
     matplotlib.pyplot.show()
 
+def get_file_index(images, pattern):
+    if pattern.isdigit() and int(pattern) < len(images):
+        return int(pattern)
+    for i, path in enumerate(images):
+        if fnmatch.fnmatch(path, pattern):
+            return i
+    logger.error('Found no image matching {}'.format(pattern))
+    return None
+
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Calculate image features')
+    parser = argparse.ArgumentParser(description='Calculate image features',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-v', '--verbose', action='count')
     parser.add_argument('-i', '--images_path', default='.',
                         help='The directory with photos')
@@ -123,8 +135,10 @@ def _main():
     images, pca_features = get_pca_features(args.images_path, args.pca_features_file)
 
     if args.index:
-        show_closest_images(pca_features, images, int(args.index))
-    
-                      
+        ix = get_file_index(images, args.index)
+        if ix is not None:
+            show_closest_images(pca_features, images, ix)
+
+
 if __name__ == '__main__':
     _main()
