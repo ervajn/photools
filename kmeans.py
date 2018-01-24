b94491613f09e4beb18a216c7807d60e561bbb1f
Åke Johansson <akejo@telia.com>
2018-01-24 23:15:29 +0100

    Calculating medoids properly, i.e. per class

--- kmeans.py
+++ kmeans.py
@@ -13,16 +13,23 @@ import sklearn.metrics
 import collections
 
 def do_kmeans(features, n_clusters):
+    logging.debug('Running kmeans. len(features)={}, n_clusters={}'.format(len(features), n_clusters))
     kmeans = sklearn.cluster.KMeans(n_clusters=n_clusters, random_state=0).fit(features)
     membership = kmeans.predict(features)
-    medoids, _ = sklearn.metrics.pairwise_distances_argmin_min(kmeans.cluster_centers_, features)
-    duplicates = [k for k,v in collections.Counter(medoids).items() if v>1]
-    if duplicates:
-        logging.info('Duplicates: {}'.format([(d, [i for i, x in enumerate(medoids.tolist()) if x == d]) for d in duplicates]))
+    class_sizes = [membership.tolist().count(x) for x in set(membership)]
+    logging.debug('Class sizes: smallest: {}, largest: {}'.format(min(class_sizes), max(class_sizes)))
+    medoids = []
+    logging.debug('Looking for medoids in {} classes'.format(n_clusters))
+    for class_ in range(n_clusters):
+        class_members = [i for i, c in enumerate(membership) if c == class_]
+        class_features = [features[i] for i in class_members]
+        medoid, _ = sklearn.metrics.pairwise_distances_argmin_min([kmeans.cluster_centers_[class_]], class_features)
+        medoids.append(class_members[medoid[0]])
+    assert [k for k,v in collections.Counter(medoids).items() if v>1] == [], "duplicates in medoids"
     return membership, medoids
 
 def plot_grid(images, texts, tile_size):
-    font_height = 5
+    font_height = 8
     font = PIL.ImageFont.truetype("/usr/share/fonts/truetype/ubuntu-font-family/Ubuntu-B.ttf", font_height)
     n = int(math.ceil(math.sqrt(len(images))))
     full_size = tile_size * n
@@ -48,6 +55,8 @@ def parse_arguments():
     parser.add_argument('-v', '--verbose', action='count')
     parser.add_argument('-p', '--pca_features_file', default='pca_features.p',
                         help='The PCA features file name')
+    parser.add_argument('-f', '--file', default='grid.jpg',
+                        help='Resulting grid image file name')   
     parser.add_argument('-n', '--n_clusters', type=int, default=100,
                         help='Number of clusters')
     parser.add_argument('-s', '--size', type=int, default=160,
@@ -65,10 +74,11 @@ def _main():
 
     membership, medoids = do_kmeans(features=pca_features, n_clusters=args.n_clusters)
 
-    grid = plot_grid([images[x] for x in medoids],
-                     ["{} {} {}".format(i, membership.tolist().count(i), os.path.basename(images[x])) for i, x in enumerate(medoids)],
-                     args.size)
+    texts = ["{} {} {}".format(i, membership.tolist().count(i), os.path.basename(images[x])) for i, x in enumerate(medoids)]
+    grid = plot_grid([images[x] for x in medoids], texts, args.size)
 
+    logging.debug('Saving grid image to {}'.format(args.file))
+    grid.save(args.file)
     grid.show()
 
 if __name__ == '__main__':
