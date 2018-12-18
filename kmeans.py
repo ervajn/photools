2661e10b028f2c8f648a3e62cf1891569724118e
Åke Ai Johansson <ake.ai.johansson@ericsson.com>
2018-12-18 16:14:20 +0100

    Fixing hash function by handling 2.7 and 3.5 differently
    Change-Id: I341154a55650131387a67c7f1133e4266f4445fb

--- kmeans.py
+++ kmeans.py
@@ -2,6 +2,7 @@
 from __future__ import print_function, division
 import argparse
 import copy
+import hashlib
 import logging
 import math
 import sys
@@ -23,8 +24,8 @@ CACHE_DIR='./cache.tmp'
 def create_grid(images, features, n_clusters):
     if not os.path.exists(CACHE_DIR):
         os.makedirs(CACHE_DIR)
-    imstr = "".join(sorted(images))
-    path = os.path.join(CACHE_DIR, '{}_{}_{}.p'.format(n_clusters, len(images), hex(abs(hash(imstr)))[2:]))
+    imstr = "".join(sorted(images)) if sys.version_info[0] < 3 else "".join(sorted(images)).encode()
+    path = os.path.join(CACHE_DIR, '{}_{}_{}.p'.format(n_clusters, len(images), hashlib.md5(imstr).hexdigest()))
     if os.path.exists(path):
         membership, medoids, grid = pickle.load(open(path, 'rb'))
         logging.debug('Loaded {}'.format(path))
